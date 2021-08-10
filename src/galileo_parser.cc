3bf3ee46bdfac30549ce6f895d6e7d60b7ed127d
Yusuf <ytaskiran70@gmail.com>
2021-08-10 09:24:52 -0700

    devel

--- src/galileo_parser.cc
+++ src/galileo_parser.cc
@@ -1,6 +1,6 @@
 #include "galileo_parser.h"
 
-GalileoParser::GalileoParser(const std::string& path) : file_(path) {}
+GalileoParser::GalileoParser(const std::string &path) : file_(path) {}
 
 void GalileoParser::Read() {
   raw_data_.open(file_, std::ios::binary);
@@ -10,12 +10,13 @@ void GalileoParser::Read() {
   }
 
   while (!raw_data_.eof()) {
-    raw_data_.read(reinterpret_cast<char*>(&byte_), sizeof(byte_));
+    raw_data_.read(reinterpret_cast<char *>(&byte_), sizeof(byte_));
 
     CheckSyncHeaders(byte_);
 
     if (sync_lock_1_ && sync_lock_2_) {
-      ParseInitialData(raw_data_, msg_type);
+      counter++;
+      ParseInitialData(raw_data_, msg_type_);
       ParsePayloadData(raw_data_);
 
       sync_lock_1_ = false;
@@ -26,7 +27,7 @@ void GalileoParser::Read() {
   raw_data_.close();
 }
 
-void GalileoParser::CheckSyncHeaders(uint8_t& byte_) {
+void GalileoParser::CheckSyncHeaders(uint8_t &byte_) {
   if (!sync_lock_1_) {
     if (byte_ == SYNC_HEADER_1_) {
       sync_lock_1_ = true;
@@ -41,143 +42,262 @@ void GalileoParser::CheckSyncHeaders(uint8_t& byte_) {
 
 /*
 Belki aşağıdaki fonksiyondan length verisi alınarak
-eğer istenilen message type değilse hızlı ilerlemek için 
+eğer istenilen message type değilse hızlı ilerlemek için
 bir byte bir byte ilerlemesi yerine hızlı bir şekilde
 seekg ile falan ilerletilebilir
 */
 
-void GalileoParser::ParseInitialData(std::ifstream& raw_data_, MessageType& msg_type) {
-  raw_data_.read(reinterpret_cast<char*>(&msg_data),
-                 sizeof(msg_data));
+void GalileoParser::ParseInitialData(std::ifstream &raw_data_,
+                                     MessageType &msg_type_) {
+  raw_data_.read(reinterpret_cast<char *>(&msg_data), sizeof(msg_data));
 
-  if (msg_data.message_class == 0x02 && msg_data.message_id == 0x13){
-    msg_type = UBX_RXM_SFRBX;
+  if (msg_data.message_class == 0x02 && msg_data.message_id == 0x13) {
+    msg_type_ = UBX_RXM_SFRBX;
     rxm_sfrbx_counter++;
-  }
-  else if (msg_data.message_class == 0x01 && msg_data.message_id == 0x43){
-    msg_type = UBX_NAV_SIG;
+  } else if (msg_data.message_class == 0x01 && msg_data.message_id == 0x43) {
+    msg_type_ = UBX_NAV_SIG;
     nav_sig_counter++;
-  } 
-  else msg_type = NOT_DEFINED;
+  } else
+    msg_type_ = NOT_DEFINED;
 }
 
-void GalileoParser::ParsePayloadData(std::ifstream& raw_data_) {
-  if (msg_type == UBX_RXM_SFRBX){
-    raw_data_.read(reinterpret_cast<char*>(&payload_sfrbx_head), sizeof(payload_sfrbx_head));
+bool GalileoParser::ParsePayloadData(std::ifstream &raw_data_) {
+  if (msg_type_ == UBX_RXM_SFRBX) {
+    raw_data_.read(reinterpret_cast<char *>(&payload_sfrbx_head),
+                   sizeof(payload_sfrbx_head));
+
     GnssCount(payload_sfrbx_head);
+
+    if (payload_sfrbx_head.gnssId != 2)
+      return false;
+
+    raw_data_.read(reinterpret_cast<char *>(&payload_data_word_head),
+                   sizeof(payload_data_word_head));
+
+    if (payload_data_word_head.page_type == 1) // Skip alert pages
+      return false;
+
+    bool check_word_type = DetermineWordType(payload_data_word_head);
+
+    if (!check_word_type)
+      return false;
+
+    ParseDataWord(raw_data_);
+
+    return true;
   }
-  
-  else if (msg_type == UBX_NAV_SIG){
-    raw_data_.read(reinterpret_cast<char*>(&payload_navsig_head), sizeof(payload_navsig_head));
-    
-    for (int i=0; i<payload_navsig_head.numSigs; i++){      
-      raw_data_.read(reinterpret_cast<char*>(&payload_navsig), sizeof(payload_navsig));
+
+  else if (msg_type_ == UBX_NAV_SIG) {
+    raw_data_.read(reinterpret_cast<char *>(&payload_navsig_head),
+                   sizeof(payload_navsig_head));
+
+    for (int i = 0; i < payload_navsig_head.numSigs; i++) {
+      raw_data_.read(reinterpret_cast<char *>(&payload_navsig),
+                     sizeof(payload_navsig));
       GnssCount(payload_navsig);
     }
+    return true;
+  }
+  return false;
+}
+
+bool GalileoParser::DetermineWordType(
+    NavigationDataWordHead &payload_data_word_head) {
+  switch (payload_data_word_head.word_type) {
+  case 0:
+    word_type_ = SPARE;
+    return true;
+
+  case 1:
+    word_type_ = EPHEMERIS_1;
+    return true;
+
+  case 2:
+    word_type_ = EPHEMERIS_2;
+    return true;
+
+  case 3:
+    word_type_ = EPHEMERIS_3;
+    return true;
+
+  case 4:
+    word_type_ = EPHEMERIS_4__CLOCK_CORRECTION;
+    return true;
+
+  case 5:
+    word_type_ = IONOSPHERIC_CORRECTION__BGD__SIG_HEALTH__DVS__GST;
+    return true;
+
+  case 6:
+    word_type_ = GST_UTC_CONVERSION;
+    return true;
+
+  case 7:
+    word_type_ = ALMANAC_1;
+    return true;
+
+  case 8:
+    word_type_ = ALMANAC_2;
+    return true;
+
+  case 9:
+    word_type_ = ALMANAC_3;
+    return true;
+
+  case 10:
+    word_type_ = ALMANAC_4;
+    return true;
+
+  case 16:
+    word_type_ = REDUCED_CED;
+    return true;
+
+  case 17:
+  case 18:
+  case 19:
+  case 20:
+    word_type_ = FEC2;
+    return true;
+
+  case 63:
+    word_type_ = DUMMY;
+    return true;
+
+  default:
+    // Warn();
+    return false;
   }
 }
 
-void GalileoParser::GnssCount(NavigationDataHead& payload){
-  switch (payload.gnssId){
-
-    case 0:
-      if (msg_type == UBX_RXM_SFRBX) gps_num_sfrbx_++;
-      else Warn();
-      break;
-
-    case 1:
-      if (msg_type == UBX_RXM_SFRBX) sbas_num_sfrbx_++;
-      else Warn();
-      break;
-
-    case 2:
-      if (msg_type == UBX_RXM_SFRBX) galileo_num_sfrbx_++;
-      else Warn();
-      break;
-
-    case 3:
-      if (msg_type == UBX_RXM_SFRBX) beidou_num_sfrbx_++;
-      else Warn();
-      break;
-
-    case 5:
-      if (msg_type == UBX_RXM_SFRBX) qzss_num_sfrbx_++;
-      else Warn();
-      break;
-
-    case 6:
-      if (msg_type == UBX_RXM_SFRBX) glonass_num_sfrbx_++;
-      else Warn();
-      break;
-
-    default:
-      std::cout << "WARNING!!!" << std::endl;
-      break;
-  } 
+void GalileoParser::ParseDataWord(std::ifstream &raw_data_) {
+  if (word_type_ == EPHEMERIS_1){
+    
+  }
 }
 
+void GalileoParser::GnssCount(NavigationDataHead &payload) {
+  switch (payload.gnssId) {
 
-void GalileoParser::GnssCount(SignalInformation& payload){
-  switch (payload.gnssId){
-
-    case 0:
-      if (msg_type == UBX_NAV_SIG) gps_num_navsig_++;
-      else Warn();
-      break;
-
-    case 1:
-      if (msg_type == UBX_NAV_SIG) sbas_num_navsig_++;
-      else Warn();
-      break;
-
-    case 2:
-      if (msg_type == UBX_NAV_SIG) galileo_num_navsig_++;
-      else Warn();
-      break;
-
-    case 3:
-      if (msg_type == UBX_NAV_SIG) beidou_num_navsig_++;
-      else Warn();
-      break;
-
-    case 5:
-      if (msg_type == UBX_NAV_SIG) qzss_num_navsig_++;
-      else Warn();
-      break;
-
-    case 6:
-      if (msg_type == UBX_NAV_SIG) glonass_num_navsig_++;
-      else Warn();
-      break;
-
-    default:
+  case 0:
+    if (msg_type_ == UBX_RXM_SFRBX)
+      gps_num_sfrbx_++;
+    else
       Warn();
-      break;
+    break;
+
+  case 1:
+    if (msg_type_ == UBX_RXM_SFRBX)
+      sbas_num_sfrbx_++;
+    else
+      Warn();
+    break;
+
+  case 2:
+    if (msg_type_ == UBX_RXM_SFRBX)
+      galileo_num_sfrbx_++;
+    else
+      Warn();
+    break;
+
+  case 3:
+    if (msg_type_ == UBX_RXM_SFRBX)
+      beidou_num_sfrbx_++;
+    else
+      Warn();
+    break;
+
+  case 5:
+    if (msg_type_ == UBX_RXM_SFRBX)
+      qzss_num_sfrbx_++;
+    else
+      Warn();
+    break;
+
+  case 6:
+    if (msg_type_ == UBX_RXM_SFRBX)
+      glonass_num_sfrbx_++;
+    else
+      Warn();
+    break;
+
+  default:
+    Warn();
+    break;
   }
 }
 
+void GalileoParser::GnssCount(SignalInformation &payload) {
+  switch (payload.gnssId) {
 
-void GalileoParser::Log(){
+  case 0:
+    if (msg_type_ == UBX_NAV_SIG)
+      gps_num_navsig_++;
+    else
+      Warn();
+    break;
+
+  case 1:
+    if (msg_type_ == UBX_NAV_SIG)
+      sbas_num_navsig_++;
+    else
+      Warn();
+    break;
+
+  case 2:
+    if (msg_type_ == UBX_NAV_SIG)
+      galileo_num_navsig_++;
+    else
+      Warn();
+    break;
+
+  case 3:
+    if (msg_type_ == UBX_NAV_SIG)
+      beidou_num_navsig_++;
+    else
+      Warn();
+    break;
+
+  case 5:
+    if (msg_type_ == UBX_NAV_SIG)
+      qzss_num_navsig_++;
+    else
+      Warn();
+    break;
+
+  case 6:
+    if (msg_type_ == UBX_NAV_SIG)
+      glonass_num_navsig_++;
+    else
+      Warn();
+    break;
+
+  default:
+    Warn();
+    break;
+  }
+}
+
+void GalileoParser::Log() const {
 
   std::cout << "UBX-RXM-SFRBX: " << rxm_sfrbx_counter << std::endl;
-  std::cout << "\nGalileo: " << galileo_num_sfrbx_ 
+  std::cout << "\nGalileo: " << galileo_num_sfrbx_
             << "\nGPS: " << gps_num_sfrbx_
             << "\nGLONASS: " << glonass_num_sfrbx_
             << "\nBeidou: " << beidou_num_sfrbx_
-            << "\nQZSS: " << qzss_num_sfrbx_
-            << "\nSBAS: " << sbas_num_sfrbx_ 
-            << std::endl;  
+            << "\nQZSS: " << qzss_num_sfrbx_ << "\nSBAS: " << sbas_num_sfrbx_
+            << std::endl;
 
   std::cout << "\nUBX-NAV-SIG: " << nav_sig_counter << std::endl;
-  std::cout << "\nGalileo: " << galileo_num_navsig_ 
+  std::cout << "\nGalileo: " << galileo_num_navsig_
             << "\nGPS: " << gps_num_navsig_
             << "\nGLONASS: " << glonass_num_navsig_
             << "\nBeidou: " << beidou_num_navsig_
-            << "\nQZSS: " << qzss_num_navsig_
-            << "\nSBAS: " << sbas_num_navsig_ 
-            << std::endl;  
+            << "\nQZSS: " << qzss_num_navsig_ << "\nSBAS: " << sbas_num_navsig_
+            << std::endl;
+
+  std::cout << "\nCounter: " << counter << std::endl;
+  std::cout << "True: " << true_counter << std::endl;
+  std::cout << "False: " << false_counter << std::endl;
 }
 
-void GalileoParser::Warn() {
-  std::cout << "WARNING!!!" << std::endl;
-}
+void GalileoParser::Warn() const { std::cout << "WARNING!!!" << std::endl; }
--- src/galileo_parser.h
+++ src/galileo_parser.h
@@ -1,27 +1,38 @@
+#ifndef GALILEO_GALILEO_PARSER_H
+#define GALILEO_GALILEO_PARSER_H
+
 #include <fstream>
 #include <iostream>
 #include <string>
+#include <unistd.h>
 
 class GalileoParser {
- private:
-  std::string file_;
+private:
+  const std::string file_;
   std::ifstream raw_data_;
 
   uint8_t byte_;
 
   const uint8_t SYNC_HEADER_1_ = 0xb5;
   const uint8_t SYNC_HEADER_2_ = 0x62;
-  
+
   bool sync_lock_1_ = false;
   bool sync_lock_2_ = false;
 
-  struct MessageData{
+  unsigned int counter = 0;
+  unsigned int true_counter = 0;
+  unsigned int false_counter = 0;
+
+  enum MessageType { UBX_RXM_SFRBX, UBX_NAV_SIG, NOT_DEFINED } msg_type_;
+
+  #pragma pack(1)
+  struct MessageData {
     uint8_t message_class;
     uint8_t message_id;
     uint16_t length;
   } msg_data;
 
-  struct NavigationDataHead{
+  struct NavigationDataHead {
     uint8_t gnssId;
     uint8_t svId;
     uint8_t reserved0;
@@ -31,15 +42,311 @@ class GalileoParser {
     uint8_t version;
     uint8_t reserved1;
   } payload_sfrbx_head;
+  
+  struct NavigationDataWordHead {
+    unsigned short even_odd : 1;
+    unsigned short page_type : 1;
+    unsigned short word_type : 6;
+  } payload_data_word_head;
 
-  struct SignalInformationHead{
+
+  /**
+   * @brief Word Type 1: Ephemeris (1/4)
+   * 
+   * @param reference_time t0e Ephemeris reference time
+   * @param mean_anomaly M0 Mean anomaly at reference time
+   * @param eccentricity e Eccentricity
+   * @param root_semi_major_axis A1/2 Square root of the semi-major axis
+   * 
+   */
+  struct WordType1 {
+    unsigned issue_of_data : 10;
+    unsigned reference_time : 14;
+    signed mean_anomaly : 32; 
+    unsigned eccentricity : 32;
+    unsigned root_semi_major_axis : 32; 
+    unsigned reserved : 2;
+    unsigned tail : 6;
+  };
+
+
+  /**
+   * @brief Word Type 2: Ephemeris (2/4)
+   * 
+   * @param longitude Ω0 Longitude of ascending node of
+   *                  orbital plane at weekly epoch
+   * @param inclination_angle i0 Inclination angle at reference time
+   * @param perigee ω Argument of perigee
+   * @param ia_rate_of_change i^dot Rate of change of inclination angle
+   * 
+   */
+  struct WordType2 {
+    unsigned issue_of_data : 10;
+    signed longitude : 32;
+    signed inclination_angle : 32;
+    signed perigee : 32;
+    signed ia_rate_of_change : 14;
+    unsigned reserved : 2;
+    unsigned tail : 6;
+  };
+
+
+  /**
+   * @brief Word Type 3: Ephemeris (3/4) and SISA
+   * 
+   * @param ra_rate_of_change Ω^dot Rate of change of right ascension
+   * @param mean_motion_difference ∆n Mean motion difference from computed value
+   * @param C_uc Cuc Amplitude of the cosine harmonic correction 
+   *             term to the argument of latitude
+   * @param C_us Cus Amplitude of the sine harmonic correction term 
+   *             to the argument of latitude
+   * @param C_rc Crc Amplitude of the cosine harmonic correction 
+   *             term to the orbit radius
+   * @param C_rs Crs Amplitude of the sine harmonic correction term 
+   *             to the orbit radius
+   * @param sisa SISA Signal-In-Space Accuracy
+   * 
+   */
+  struct WordType3 {
+    unsigned issue_of_data : 10;
+    signed ra_rate_of_change : 24;
+    signed mean_motion_difference : 16;
+    signed C_uc : 16;
+    signed C_us : 16;
+    signed C_rc : 16;
+    signed C_rs : 16;
+    unsigned sisa : 8;
+    unsigned tail : 6;
+  };
+
+
+  /**
+   * @brief Word Type 4: SVID, Ephemeris (4/4),
+   *        and Clock correction parameters 
+   * 
+   * @param svid Space Vehicle Identifier
+   * @param C_ic Cic Amplitude of the cosine harmonic correction 
+   *             term to the angle of inclination
+   * @param C_is Cis Amplitude of the sine harmonic correction term 
+   *             to the angle of inclination
+   * @param reference t0c Clock correction data reference Time of Week
+   * @param clock_bias_corr af0 SV clock bias correction coefficient
+   * @param clock_drift_corr af1 SV clock drift correction coefficient
+   * @param clock_drift_rate_corr af2 SV clock drift rate correction coefficient
+   * 
+   */
+  struct WordType4 {
+    unsigned issue_of_data : 10;
+    unsigned svid : 6;
+    signed C_ic : 16;
+    signed C_is : 16;
+    unsigned reference : 14;
+    signed clock_bias_corr : 31;
+    signed clock_drift_corr : 21;
+    signed clock_drift_rate_corr : 6;
+    unsigned spare : 2;
+    unsigned tail : 6;
+  };
+
+
+  /**
+   * @brief Word Type 5: Ionospheric correction, BGD, 
+   * signal health and data validity status and GST
+   * 
+   * @param effionl_0 ai0 Effective Ionisation Level 1st order parameter
+   * @param effionl_1 ai1 Effective Ionisation Level 2nd order parameter
+   * @param effionl_2 ai2 Effective Ionisation Level 3rd order parameter
+   * @param region1 SF1 Ionospheric Disturbance Flag for region 1
+   * @param region2 SF2 Ionospheric Disturbance Flag for region 2
+   * @param region3 SF3 Ionospheric Disturbance Flag for region 3
+   * @param region4 SF4 Ionospheric Disturbance Flag for region 4
+   * @param reigon5 SF5 Ionospheric Disturbance Flag for region 5
+   * @param bgd_1 BGD(E1,E5a) E1-E5a Broadcast Group Delay
+   * @param bgd_2 BGD(E1,E5b) E1-E5b Broadcast Group Delay
+   * @param sig_health_e5b E5bHS E5b Signal Health Status
+   * @param sig_health_e1 E1-BHS E1-B/C Signal Health Status
+   * @param data_validity_e5b E5bDVS E5b Data Validity Status
+   * @param data_validity_e1 E1-BDVS E1-B Data Validity Status
+   * @param week_num WN Week Number
+   * @param time_of_week TOW Time of Week
+   * 
+   */
+  struct WordType5 {
+    unsigned effionl_0 : 11;
+    signed effionl_1 : 11;
+    signed effionl_2 : 14;
+    unsigned region1 : 1;
+    unsigned region2 : 1;
+    unsigned region3 : 1;
+    unsigned region4 : 1;
+    unsigned region5 : 1;
+    signed bgd_1 : 10;
+    signed bgd_2 : 10;
+    unsigned sig_health_e5b : 2;
+    unsigned sig_health_e1 : 2;
+    unsigned data_validity_e5b : 1;
+    unsigned data_validity_e1 : 1;
+    unsigned week_num : 12;
+    unsigned time_of_week : 20;
+    unsigned spare : 23;
+    unsigned tail : 6;
+  };
+
+
+  /**
+   * @brief Word Type 6: GST-UTC conversion parameters
+   * 
+   * @param A0 A0 Constant term of polynomial
+   * @param A1 A1 1st order term of polynomial
+   * @param ls_count_before ΔtLS Leap Second count before leap
+   *                          second adjustment
+   * @param utc_reference_tow t0t UTC data reference Time of Week
+   * @param utc_reference_week WN0t UTC data reference Week Number
+   * @param WN_lsf WNLSF Week Number of leap second adjustment
+   * @param day_num DN Day Number at the end of which a leap second 
+   *                adjustment becomes effective. The value range of 
+   *                DN is from 1 (= Sunday) to 7 (= Saturday).
+   * @param ls_count_after ΔtLSF Leap Second count after leap second adjustment
+   * @param time_of_week TOW Time of Week
+   * 
+   */
+  struct WordType6 {
+    signed A0 : 32;
+    signed A1 : 24;
+    signed ls_count_before : 8;
+    unsigned utc_reference_tow : 8;
+    unsigned utc_reference_week : 8;
+    unsigned WN_lsf : 8;
+    unsigned day_num : 3;
+    signed ls_count_after : 8;
+    unsigned time_of_week : 20;
+    unsigned spare : 8;
+    unsigned tail : 6;
+  };
+
+
+  /**
+   * @brief Almanac for SVID1 (1/2), almanac reference time and 
+   *        almanac reference week number
+   * 
+   * @param week_num WNa Almanac reference Week Number
+   * @param ref_time t0a Almanac reference time
+   * @param svid_1 SVID Satellite ID (1 constellation of 36 satellites)
+   * @param delta_root_a Δ(A1/2) Difference between the square root of the 
+   *                     semi-major axis and the square root of the 
+   *                     nominal semi-major axis      
+   * @param eccentricity Eccentricity 
+   * @param perigee ω Argument of perigee
+   * @param diff_ia_na δi Difference between the inclination angle at 
+   *                   reference time and the nominal inclination 
+   * @param longitude Ω0 Longitude of ascending node of orbital plane 
+   *                  at weekly epoch
+   * @param roc_ra Ω^dot Rate of change of right ascension
+   * @param mean_anomaly M0 Satellite mean anomaly at reference time
+   * 
+   */
+  struct WordType7 {
+    unsigned issue_of_data : 4;
+    unsigned week_num : 2;
+    unsigned ref_time : 10;
+    unsigned svid_1 : 6;
+    signed delta_root_a : 13;
+    unsigned eccentricity : 11;
+    signed perigee : 16;
+    signed diff_ia_na : 11;
+    signed longitude : 16;
+    signed roc_ra : 11;
+    signed mean_anomaly : 16;
+    unsigned reserved : 6;
+    unsigned tail : 6;
+  };
+
+
+  /**
+   * @brief Word Type 8: Almanac for SVID1 (2/2) and SVID2 (1/2))
+   * 
+   * @param clock_corr_bias af0 Satellite clock correction bias “truncated”
+   * @param clock_corr_linear af1 Satellite clock correction linear “truncated”
+   * @param sig_health_e5b E5bHS Satellite E5b signal health status 
+   * @param sig_health_e1 E1-BHS Satellite E1-B/C signal health status
+   * @param svid_2 SVID Satellite ID (1 constellation of 36 satellites)
+   * @param delta_root_a Δ(A1/2) Difference between the square root of the 
+   *                   semi-major axis and the square root of the 
+   *                   nominal semi-major axis
+   * @param eccentricity e Eccentricity
+   * @param perigee ω Argument of perigee
+   * @param diff_ia_na δi Difference between the inclination angle at 
+   *                   reference time and the nominal inclination
+   * @param longitude Ω0 Longitude of ascending node of orbital plane 
+   *                  at weekly epoch   
+   * @param roc_ra Ω^dot Rate of change of right ascension  
+   * 
+   */
+  struct WordType8 {
+    unsigned issue_of_data : 4;
+    signed clock_corr_bias : 16;
+    signed clock_corr_linear : 13;
+    unsigned sig_health_e5b : 2;
+    unsigned sig_health_e1 : 2;
+    unsigned svid_2 : 6;
+    signed delta_root_a : 13;
+    unsigned eccentricity : 11;
+    signed perigee : 16;
+    signed diff_ia_na : 11;
+    signed longitude : 16;
+    signed roc_ra : 11;
+    unsigned spare : 1;
+    unsigned tail : 6;
+  };
+
+
+  /**
+   * @brief Word Type 9: Almanac for SVID2 (2/2) and SVID3 (1/2))
+   * 
+   * @param week_num WNa Almanac reference Week Number
+   * @param ref_time t0a Almanac reference time
+   * @param mean_anomaly M0 Satellite mean anomaly at reference time
+   * @param clock_corr_bias af0 Satellite clock correction bias “truncated”
+   * @param clock_corr_linear af1 Satellite clock correction linear “truncated”
+   * @param sig_health_e5b Satellite E5b signal health status
+   * @param sig_health_e1 E1-BHS Satellite E1-B/C signal health status
+   * @param svid_3 SVID Satellite ID (1 constellation of 36 satellites)
+   * @param delta_root_a Δ(A1/2) Difference between the square root of the 
+   *                     semi-major axis and the square root of the 
+   *                     nominal semi-major axis
+   * @param eccentricity e Eccentricity
+   * @param perigee ω Argument of perigee
+   * @param diff_ia_na δi Difference between the inclination angle at 
+   *                   reference time and the nominal inclination
+   * 
+   */
+  struct WordType9 {
+    unsigned issue_of_data : 4;
+    unsigned week_num : 2;
+    unsigned ref_time : 10;
+    signed mean_anomaly : 16;
+    signed clock_corr_bias : 16;
+    signed clock_corr_linear : 13;
+    unsigned sig_health_e5b : 2;
+    unsigned sig_health_e1 : 2;
+    unsigned svid_3 : 6;
+    signed delta_root_a : 13;
+    unsigned eccentricity : 11;
+    signed perigee : 16;
+    signed diff_ia_na : 11;
+    unsigned tail : 6;
+  };
+
+
+
+  struct SignalInformationHead {
     uint32_t iTOW;
     uint8_t version;
     uint8_t numSigs;
     uint16_t reserved0;
   } payload_navsig_head;
 
-  struct SignalInformation{
+  struct SignalInformation {
     uint8_t gnssId;
     uint8_t svId;
     uint8_t sigId;
@@ -52,14 +359,24 @@ class GalileoParser {
     unsigned int sigFlags : 16;
     uint32_t reserved1;
   } payload_navsig;
+  #pragma pack()
 
-  enum MessageType {
-    UBX_RXM_SFRBX,
-    UBX_NAV_SIG,
-    NOT_DEFINED
-  } msg_type;
-
-  uint32_t words[9];
+  enum WordType {
+    SPARE,
+    EPHEMERIS_1,
+    EPHEMERIS_2,
+    EPHEMERIS_3,
+    EPHEMERIS_4__CLOCK_CORRECTION,
+    IONOSPHERIC_CORRECTION__BGD__SIG_HEALTH__DVS__GST,
+    GST_UTC_CONVERSION,
+    ALMANAC_1,
+    ALMANAC_2,
+    ALMANAC_3,
+    ALMANAC_4,
+    REDUCED_CED = 16,
+    FEC2, 
+    DUMMY = 63
+  }word_type_;
 
   unsigned int galileo_num_sfrbx_ = 0;
   unsigned int gps_num_sfrbx_ = 0;
@@ -78,16 +395,19 @@ class GalileoParser {
   unsigned int rxm_sfrbx_counter = 0;
   unsigned int nav_sig_counter = 0;
 
-
- public:
+public:
   explicit GalileoParser(const std::string &path);
-  
+
   void Read();
-  void CheckSyncHeaders(uint8_t& byte_);
-  void ParseInitialData(std::ifstream& raw_data_, MessageType& msg_type);
-  void ParsePayloadData(std::ifstream& raw_data_);
-  void GnssCount(NavigationDataHead& payload);
-  void GnssCount(SignalInformation& payload);
-  void Log();
-  void Warn();
+  void CheckSyncHeaders(uint8_t &byte_);
+  void ParseInitialData(std::ifstream &raw_data_, MessageType &msg_type);
+  bool ParsePayloadData(std::ifstream &raw_data_);
+  void ParseDataWord(std::ifstream &raw_data_);
+  bool DetermineWordType(NavigationDataWordHead &payload_data_word_head);
+  void GnssCount(NavigationDataHead &payload);
+  void GnssCount(SignalInformation &payload);
+  void Log() const;
+  void Warn() const;
 };
+
+#endif // GALILEO_GALILEO_PARSER_H
